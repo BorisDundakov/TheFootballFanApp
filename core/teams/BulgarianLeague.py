e50caaee33f663faae3bef45e0b520f633274fcb
Boris Dundakov <boris.stefan.de@gmail.com>
2022-08-25 14:31:00 +0300

    next fixture

--- core/teams/BulgarianLeague.py
+++ core/teams/BulgarianLeague.py
@@ -1,5 +1,5 @@
+from bs4 import BeautifulSoup
 import requests
-
 
 new_url = "https://prod-public-api.livescore.com/v1/api/app/stage/soccer/bulgaria/parva-liga/3?MD=1"
 old_url = "https://www.livescore.com/en/football/bulgaria/parva-liga/table/"
@@ -8,7 +8,7 @@ old_url = "https://www.livescore.com/en/football/bulgaria/parva-liga/table/"
 def export_team_names():
     url = "https://prod-public-api.livescore.com/v1/api/app/stage/soccer/bulgaria/parva-liga/3?MD=1"
 
-    football_clubs = []
+    football_clubs = {}
 
     response = requests.get(url)
     # soup = BeautifulSoup(page.content, 'html.parser')
@@ -17,9 +17,28 @@ def export_team_names():
 
     for index in teams:
         team_name = index['Tnm']
+        team_number = index['Tid']
         team_name.capitalize()
-        football_clubs.append(team_name)
+        football_clubs[team_number] = team_name
 
     return football_clubs
 
+
+def export_next_fixture(team_name, team_number):
+    team = team_name.lower()
+    team = team.replace(" ", "-")
+    # https://www.livescore.com/en/football/team/levski-sofia/6552/overview/
+    # https://www.livescore.com/en/football/team/team-name/1111/overview/
+    result = {"home": 0, "away": 0}
+    url = "https://www.livescore.com/en/football/team/" + f"{team}" + f"/{team_number}/" + "overview/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    team_names = soup.find_all('span', class_='li')
+    for team_name in range(len(team_names)):
+        if team_name == 0:
+            result['home'] = team_names[team_name].text
+        else:
+            result['away'] = team_names[team_name].text
+
+    return result
 
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from core.teams.BulgarianLeague import export_team_names
+from core.teams.BulgarianLeague import export_next_fixture
 
 
 # Create your views here.
@@ -11,11 +12,20 @@ def frontpage(request):
 def team(request):
     context = {}
     team_name = request.POST.get('team_name')
-    #team_name = team_name.capitalize()
+    # team_name = team_name.capitalize()
     context['team_name'] = team_name
-    # TODO: Teams web scraping!
 
     teams = export_team_names()
-    if team_name not in teams:
+    if team_name not in teams.values():
         return render(request, 'frontpage.html')
+
+    team_number = 0
+    for key, value in teams.items():
+        if value == team_name:
+            team_number = key
+            break
+
+    result = export_next_fixture(team_name, team_number)
+    context['next_fixture'] = result
+
     return render(request, 'team.html', context)
